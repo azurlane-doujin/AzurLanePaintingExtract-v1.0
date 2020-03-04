96cd18b350a22db91a5ded4347fd43484597148b
凊弦凝绝 <42119608+Goodjooy@users.noreply.github.com>
2020-03-04 16:49:32 +0800

    Add files via upload

--- core/src/thread_classes/extract_thread.py
+++ core/src/thread_classes/extract_thread.py
@@ -38,78 +38,80 @@ class RestoreThread(threading.Thread):
 
             if self.stop:
                 return
-            if self.index < len(self.able) and not self.stop:
-                now_info: PerInfo = self.able[self.index]
-                self.format.m_staticText_info.SetLabel("当前第%d个！为：%s 类型-直接还原" % (self.index + 1, now_info.cn_name))
+            try:
+                if self.index < len(self.able) and not self.stop:
+                    now_info: PerInfo = self.able[self.index]
+                    self.format.m_staticText_info.SetLabel("当前第%d个！为：%s 类型-直接还原" % (self.index + 1, now_info.cn_name))
 
-                now_info.is_save_as_cn = self.setting[data.sk_use_cn_name]
+                    now_info.is_save_as_cn = self.setting[data.sk_use_cn_name]
 
-                # 文件分类部分
-                if self.setting[data.sk_output_group] == data.feg_do_no_group:
-                    save_path = self.save_path
+                    # 文件分类部分
+                    if self.setting[data.sk_output_group] == data.feg_do_no_group:
+                        save_path = self.save_path
 
-                elif self.setting[data.sk_output_group] == data.feg_by_type:
-                    pattern_skin = data.fp_skin
-                    pattern_power = data.fp_build_up
-                    pattern_marry = data.fp_wedding
-                    pattern_young = data.fp_young
-                    pattern_self = data.fp_default_skin
-                    if pattern_skin.match(now_info.name) is not None:
+                    elif self.setting[data.sk_output_group] == data.feg_by_type:
+                        pattern_skin = data.fp_skin
+                        pattern_power = data.fp_build_up
+                        pattern_marry = data.fp_wedding
+                        pattern_young = data.fp_young
+                        pattern_self = data.fp_default_skin
+                        if pattern_skin.match(now_info.name) is not None:
 
-                        save_path = f"{self.save_path}\\皮肤"
+                            save_path = f"{self.save_path}\\皮肤"
 
-                    elif pattern_marry.match(now_info.name) is not None:
-                        save_path = f"{self.save_path}\\婚纱"
+                        elif pattern_marry.match(now_info.name) is not None:
+                            save_path = f"{self.save_path}\\婚纱"
 
-                    elif pattern_power.match(now_info.name) is not None:
-                        save_path = f"{self.save_path}\\改造"
+                        elif pattern_power.match(now_info.name) is not None:
+                            save_path = f"{self.save_path}\\改造"
 
-                    elif pattern_self.match(now_info.name) is not None:
-                        save_path = f"{self.save_path}\\原皮"
+                        elif pattern_self.match(now_info.name) is not None:
+                            save_path = f"{self.save_path}\\原皮"
 
-                    elif pattern_young.match(now_info.name) is not None:
-                        save_path = f"{self.save_path}\\幼女化"
+                        elif pattern_young.match(now_info.name) is not None:
+                            save_path = f"{self.save_path}\\幼女化"
 
-                    elif data.fp_u_skin.match(now_info.name) is not None:
-                        save_path = f"{self.save_path}\\μ兵装"
+                        elif data.fp_u_skin.match(now_info.name) is not None:
+                            save_path = f"{self.save_path}\\μ兵装"
 
-                    else:
-                        save_path = f"{self.save_path}\\其他"
-
-                elif self.setting[data.sk_output_group] == data.feg_by_name:
-                    val = re.match(r'^(.+)(_[hg\d])$', now_info.name)
-                    if val is not None:
-                        val = val.group(1)
-                        if now_info.has_cn and self.setting[data.sk_use_cn_name]:
-                            value = self.names[val]
-                            if value != "":
-                                val = value
-                    else:
-                        if now_info.has_cn and self.setting[data.sk_use_cn_name]:
-                            val = now_info.cn_name
                         else:
-                            val = now_info.name
+                            save_path = f"{self.save_path}\\其他"
 
-                    save_path = f"{self.save_path}\\{val}"
+                    elif self.setting[data.sk_output_group] == data.feg_by_name:
+                        val = re.match(r'^(.+)(_[hg\d])$', now_info.name)
+                        if val is not None:
+                            val = val.group(1)
+                            if now_info.has_cn and self.setting[data.sk_use_cn_name]:
+                                value = self.names[val]
+                                if value != "":
+                                    val = value
+                        else:
+                            if now_info.has_cn and self.setting[data.sk_use_cn_name]:
+                                val = now_info.cn_name
+                            else:
+                                val = now_info.name
 
-                elif self.setting[data.sk_output_group] == data.feg_by_is_able:
-                    save_path = f"{self.save_path}\\{'还原'}"
+                        save_path = f"{self.save_path}\\{val}"
 
-                else:
-                    save_path = self.save_path
+                    elif self.setting[data.sk_output_group] == data.feg_by_is_able:
+                        save_path = f"{self.save_path}\\{'还原'}"
 
-                os.makedirs(save_path, exist_ok=True)
+                    else:
+                        save_path = self.save_path
 
-                now_info.add_save(save_path)
+                    os.makedirs(save_path, exist_ok=True)
 
-                is_good, info = ImageWork.restore_tool(now_info)
-                if not is_good:
-                    self.format.m_staticText_info.SetLabel(info)
+                    now_info.add_save(save_path)
 
-                val = round(100 * (self.index / len(self.able)))
-                self.format.m_gauge_state.SetValue(val)
-                self.index += 1
+                    is_good, info = ImageWork.restore_tool(now_info)
+                    if not is_good:
+                        self.format.m_staticText_info.SetLabel(info)
 
+                    val = round(100 * (self.index / len(self.able)))
+                    self.format.m_gauge_state.SetValue(val)
+                    self.index += 1
+            except Exception as info:
+                self.format.m_staticText_info.SetLabel(f"处理出错！，为{info}")
         if self.stop:
             return
         if self.setting[data.sk_export_all_while_copy]:
